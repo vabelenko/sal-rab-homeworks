15425bdd6f6109d5ba22bd6bc3a237c8c25bea87
Vladimir Belenko <vabelenko@mail.ru>
2022-04-04 21:55:41 +0300

    Выполнил задание 1

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,9 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+    let message = productName + " " + "за" + " " + productPrice + " " + "теперь в корзине!";
+    console.log(message);
+
     // Конец решения задания №1.1.
 
     return message;
@@ -27,6 +30,8 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+    
+    let newValue = oldValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -42,7 +47,10 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ` ₽`;
+    
     // Конец решения задания №1.3.
 
     return newSumText;
