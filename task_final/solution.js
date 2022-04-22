2292e0a2c742dd9ed8d060dcc6e9a73e29db28ed
Vladimir Belenko <vabelenko@mail.ru>
2022-04-22 19:36:04 +0300

    Итоговое задание.

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,33 @@
 //    }
 // }
 
+
+
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    
 
-    let countOfGoods = goods.length;
+let data = {data: {client: " ", order: {address: "", sum: 0} , 
+    goods: [] } 
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+           };    
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+data.data.client =  `${name} ${phone}`  
+data.data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;  
+ 
+data.data.order.sum = sum;
 
-    data.client = 'Иван';
+let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i += 1) {
+    data.data.goods.push({title:goods[i].title, count: goods[i].count});
+    };
 
-    let jsonData = JSON.stringify(data);
 
-    return jsonData;
+ 
+let jsonData = JSON.stringify(data);
+
+return jsonData;
 }
+
+
+   
